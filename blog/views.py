f2aaa520502b3ddd4f18d112623f96b545c8e29a
Victoria Brock <victoriabrock@geniesolutions.com.au>
2019-07-13 15:47:23 +1000

    Deploying Post templates, using HTML

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
+#Add models to view. The '.' before models works because it's in the same directory as the views file.
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 #Making a view...something
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+    
+
+
+
